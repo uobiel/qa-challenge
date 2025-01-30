3051986c8fc9ada3d1c04f1b01bec1f655abee33
uobiel <gabrielantunesemp@gmail.com>
2025-01-30 18:16:23 -0300

    FIX: HEADLESS ON

--- playwright.config.ts
+++ playwright.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   use: {
-    headless: false,  
+    headless: true,  
     viewport: { width: 1280, height: 720 },
     screenshot: 'on', 
     video: 'retain-on-failure', 
